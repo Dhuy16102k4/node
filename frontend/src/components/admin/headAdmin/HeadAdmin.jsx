c47ad1ace695db98446f9eed7976b2564fc7dd59
Huy Ngô <121811007+Hyun177@users.noreply.github.com>
2024-12-10 21:53:52 +0700

    ádDSa

--- frontend/src/components/admin/headAdmin/HeadAdmin.jsx
+++ frontend/src/components/admin/headAdmin/HeadAdmin.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import SettingsIcon from "@mui/icons-material/Settings"
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
+
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined"
 import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined"
 import { assets } from "../../../assets/assets"
@@ -16,10 +16,7 @@ const HeadAdmin = ({ dark, setMode }) => {
             </div>
           </div>
           <div className='right flexCenter'>
-            <div className='search flexCenter'>
-              <input type='text' placeholder='Search...' />
-              <SearchOutlinedIcon className='iconHead' />
-            </div>
+            
             <NotificationsNoneOutlinedIcon className='iconHead' />
             <div className='profile flexCenter'>
               <img className='imageCircle' src='https://png.pngtree.com/png-clipart/20190520/original/pngtree-vector-users-icon-png-image_4144740.jpg' alt='' />
